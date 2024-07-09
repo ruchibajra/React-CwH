45d8ee14ee59b9160e5d82017b61b288ca8d2a0f
ruchibajra <ruchibajracharya8@gmail.com>
2024-07-09 22:44:42 +0545

    understanding state and handling event

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  //on change is necessary as it helps to write on the text area
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("Enter the text");
+
   return (
     <div>
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
-        </div>
-        <button className="div div-primary">Convert to uppercase</button>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          id="myBox"
+          value={text}
+          onChange={handleOnChange}
+          rows="8"
+        ></textarea>
+      </div>
+      <button className="div div-primary" onClick={handleUpClick}>
+        Convert to uppercase
+      </button>
     </div>
-  )
+  );
 }
